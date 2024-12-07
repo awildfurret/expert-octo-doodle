bde3a1321f65ea734275772bd2ab156b0187a25c
awildfurret <matth.alexander88@gmail.com>
2024-12-07 15:55:20 +0700

    Idk help me fix this lol

--- pythonProject1/skibidi.py
+++ pythonProject1/skibidi.py
@@ -1,1 +1,9 @@
 print ("Erm, what the Sigma")
+
+def skibidi():
+sigma = input("Skibidi Ohio Rizz? ")
+
+if sigma = None:
+    print("Very sigma, no need to say a thing")
+else:
+    print("Nuh uh")
